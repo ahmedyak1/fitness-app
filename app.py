7817bcfe570c24aa84068f3b858134135e755a35
ahmed yakine <ahmedmac@MacBook-Pro-de-ahmed.local>
2024-03-15 03:40:31 +0000

    Merge remote changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, redirect, url_for, session, request
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-# Dummy data for illustration purposes
+
 users = {'admin': 'password'}
 activities = [{'type': 'Run', 'distance': 5, 'date': '2024-03-16'}]
 
@@ -36,13 +36,13 @@ def log_activity():
         return redirect(url_for('login'))
     
     if request.method == 'POST':
-        # Simplified example; in a real app, you'd save this data to a database
+        
         activity = {
             'type': request.form['type'],
             'distance': request.form['distance'],
             'date': request.form['date']
         }
-        activities.append(activity)  # Assuming 'activities' is a global list
+        activities.append(activity)  
         return redirect(url_for('home'))
     
     return render_template('log_activity.html')
